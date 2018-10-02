84c45fe0512aaa1c06209e9ab765595554c41843
Jacob Presley <jacobpresley32@gmail.com>
2018-10-01 20:34:36 -0400

    Rough Camera is working

--- TopDownMulti/Assets/PlayerCameraScript.cs
+++ TopDownMulti/Assets/PlayerCameraScript.cs
@@ -25,6 +25,9 @@ public class PlayerCameraScript : MonoBehaviour {
     Vector3 smoothVelocity;
     public Transform[] targets;
 
+    float xMax = 5.0f;
+    float zMax = 5.0f;
+
     Vector3 newPos = new Vector3();
     // Use this for initialization
     void Start()
@@ -41,6 +44,61 @@ public class PlayerCameraScript : MonoBehaviour {
         //transform.position = newPos;
         //transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
         CenterCamOnTargets();
+        CameraZoom();
+    }
+
+    void CenterCamOnTargets()
+    {
+        //var bounds = new Bounds(target.transform.position, Vector3.zero);
+
+        //bounds.Encapsulate(mousePos);
+
+        
+
+        Vector3 distPtoC;
+        distPtoC.x = transform.position.x - target.transform.position.x;
+        distPtoC.y = 0.0f;
+        distPtoC.z = transform.position.z - target.transform.position.z;
+
+        Vector3 distPtoM; //player to mouse dist
+        distPtoM.x = mousePos.x - target.transform.position.x;
+        distPtoM.y = 0.0f;
+        distPtoM.z = mousePos.z - target.transform.position.z;
+
+        Vector3 bounds;
+        bounds = distPtoM / 2.0f;
+
+        Debug.DrawLine(mousePos, target.transform.position, Color.red);
+
+        if (bounds.x > xMax)
+        {
+            bounds.x = xMax;
+        }
+
+        if (bounds.x < -xMax)
+        {
+            bounds.x = -xMax;
+        }
+
+        if (bounds.z > zMax)
+        {
+            bounds.z = zMax;
+        }
+
+        if (bounds.z < -zMax)
+        {
+            bounds.z = -zMax;
+        }
+
+        newPos.x = target.transform.position.x + bounds.x;
+        newPos.z = target.transform.position.z + bounds.z;
+        newPos.y = target.transform.position.y + yOffset;
+        //transform.position = newPos;
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref smoothVelocity, smoothTime);
+    }
+
+    void CameraZoom()
+    {
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         if (scroll > 0f)
         {
@@ -60,46 +118,16 @@ public class PlayerCameraScript : MonoBehaviour {
         }
     }
 
-    void CenterCamOnTargets()
-    {
-        //var bounds = new Bounds(target.transform.position, Vector3.zero);
-
-        //bounds.Encapsulate(mousePos);
-
-       
-
-        Vector3 distPtoC;
-        distPtoC.x = transform.position.x - target.transform.position.x;
-        distPtoC.y = 0.0f;
-        distPtoC.z = transform.position.z - target.transform.position.z;
-
-        Vector3 distPtoM; //player to mouse dist
-        distPtoM.x = mousePos.x - target.transform.position.x;
-        distPtoM.y = 0.0f;
-        distPtoM.z = mousePos.z - target.transform.position.z;
-
-        Vector3 bounds;
-        bounds = distPtoM / 2.0f;
-
-        Debug.DrawLine(mousePos, target.transform.position, Color.red);
-
-        mag = distPtoC.magnitude;
-        if ((distPtoC).magnitude > 10.0f)
-        {
-            
-            
-        }
-        else
-        {
-            newPos.x = bounds.x + xOffset;
-            newPos.z = bounds.z + zOffset;
-            newPos.y = bounds.y + yOffset;
-            transform.position = Vector3.SmoothDamp(transform.position, newPos, ref smoothVelocity, smoothTime);
-        }
-    }
-
     public void SetMousePos(Vector3 _mousePos)
     {
         mousePos = _mousePos;
     }
+
+ //   int tempNumDays = numDays;
+ //   int tempOrganisms = organisms;
+ //   while (tempNumDays > 0)
+	//{
+ //       tempOrganisms = tempOrganisms + ((dayIncrease / 100) * tempOrganisms);
+ //       tempNumDays -= 1;
+	//}
 }
